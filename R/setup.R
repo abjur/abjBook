604af84ff2659c999b75917b17b23c0b8fa7a8fe
Julio Trecenti <julio.trecenti@gmail.com>
2017-10-31 19:43:35 -0200

    better documentation and creates folder automatically if it doesn't exist. also do not delete Rproj file

--- R/setup.R
+++ R/setup.R
@@ -1,12 +1,24 @@
 #' Setup ABJ book
 #'
-#' Setupar ABJ book
+#' Creates ABJ book file structure. It will contain all the files that you need
+#' to compile your new book. You should start by running \code{Ctrl+Shift+B}
+#' (build) on index.Rmd using RStudio. If it runs successfully, your output will
+#' be in the _book folder
 #'
-#' @param name nome do livro.
-#' @param path pasta onde os arquivos serao copiados.
+#' @param name Name of the book. You may create a folder with this name before
+#'   running this function.
+#' @param path Path to the book folder. Your book files will be inside
+#'   \code{/path/folder/name}.
 #'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' dir.create("exampleBook")
+#' abjBook::setup_abj_book("myNewBook", path = "exampleBook")
+#' }
 setup_abj_book <- function(name, path = '.') {
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
   main_folder <- find_folder(template = 'abjBook', folder = '')
 
@@ -18,7 +30,7 @@ setup_abj_book <- function(name, path = '.') {
     stringr::str_subset(pattern = "skeleton|travis") %>%
     c(folders)
 
-  file.remove(list.files(pattern = "\\.Rproj$"))
-  invisible(file.copy(copy2root, to = path, recursive = T))
+  file.remove(list.files(path, pattern = "\\.Rproj$"))
+  invisible(file.copy(copy2root, to = path, recursive = TRUE))
 
 }
